94675c08c2eb4327e4d0867faaf6314af7bcec8e
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-02-01 22:21:52 +0600

    Added switch simple practice

---
+++ switch.js
@@ -0,0 +1,38 @@
+const color = '';
+if (color == 'green'){
+    console.log('Your are a green friend');
+}
+else if (color == 'blue'){
+    console.log('You are a blue friend');
+}
+else if (color == 'red'){
+    console.log('You are a red friend');
+}
+else if (color == 'white'){
+    console.log('You are a white friend');
+}
+
+else{
+    console.log('You are a black friend');
+}
+
+
+
+
+//switch
+
+switch (color){
+    case 'green':
+        console.log('Your are a green friend');
+        break;
+    case 'red':
+        console.log('You are a red friend');
+        break;
+    case 'blue':
+        console.log('You are a blue friend');
+        break
+    case 'yellow':
+        console.log('You are a yellow friend');
+    default:
+        console.log('You are a special friend');
+}
