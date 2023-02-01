181433a19f7143dca403bc4bb65298086bfe8c94
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-02-01 15:48:14 +0600

    Added = function structure

---
+++ function.js
@@ -0,0 +1,9 @@
+// fucntion declaration
+function startFan () {
+    console.log('Stand up');
+    console.log('Walk towards the switch');
+    console.log('Press the switch');
+}
+
+// call the function
+startFan();
