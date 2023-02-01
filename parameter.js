4aa67c389f72f375e09a278eb80debc0ce9d0d71
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-02-01 16:13:55 +0600

    Added => git parameter practice

---
+++ parameter.js
@@ -0,0 +1,29 @@
+function bringSingara (money){
+    console.log('mama singara den');
+    console.log(money, 'taka disen');
+    console.log('ai nen singara');
+}
+
+
+// bringSingara(20);
+/******* or *********/
+var taka = 100;
+bringSingara(taka);
+
+
+function add (num1 , num2){
+    console.log('Going to add', num1, num2);
+}
+
+add(125, 56);
+
+
+
+
+function sum (a, b, c, d, e) {
+    console.log(a,b,c,d,e);
+    var sum = a + b + c + d + e;
+    console.log(sum);
+}
+
+sum(15, 98, 56, 5, 9);
